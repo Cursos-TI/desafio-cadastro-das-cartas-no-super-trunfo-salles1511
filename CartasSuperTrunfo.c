9d1ed9184c834ca494bd21a89c10e6280291d46a
salles1511 <renansalles123@gmail.com>
2025-05-07 23:38:22 -0300

    CartasSuperTrunfo_aventureiro.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,98 +1,83 @@
-#include<stdio.h>
-int main(){
-    char estado1;
-    char estado2;
-    char codigo_carta1[4];
-    char codigo_carta2[4];
-    char nome_cidade1[50];
-    char nome_cidade2[50];
-    int populacao1;
-    int populacao2;
-    int ponto_turistico1;
-    int ponto_turistico2;
-    float area1;
-    float area2;
-    float pib1;
-    float pib2;
+#include <stdio.h> 
 
+int main() {
+    //variáveis da primeira carta
+    char estado1[3], codigo1[4], nomeCidade1[50]; 
+    int populacao1, pontosTuristicos1;            
+    float area1, pib1;                          
+    float densidadePopulacional1, pibPerCapita1;  
 
-        printf("Você preencherá os dados para cadastrar DUAS CARTAS, Digite os dados da carta 1 e depois a carta 2. Preencha conforme os tópicos abaixo:\n");
-       
- //Inserindo os dados da PRIMEIRA carta:       
+    //variáveis da segunda carta
+    char estado2[3], codigo2[4], nomeCidade2[50];
+    int populacao2, pontosTuristicos2;
+    float area2, pib2;
+    float densidadePopulacional2, pibPerCapita2;
 
-        printf("\nCarta 1: \n");
-        printf("Digite a inicial do Estado: (Uma letra de 'A' a 'H'): \n");
-        scanf(" %c", &estado1); //o " " antes da variável garante que ele respeite a quebra de linha
+    // Recebendo os dados da carta 1
+    printf("Carta 1:\n");
+    printf("Estado: ");
+    scanf("%s", estado1);
+    printf("Codigo: ");
+    scanf("%s", codigo1);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade1);
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+    printf("Area (em km²): ");
+    scanf("%f", &area1); 
+    printf("PIB (em bilhoes de reais): ");
+    scanf("%f", &pib1); 
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos1); 
 
-        printf("Cadastre o código da Carta: (Ex: A01)\n");
-        scanf("%s", codigo_carta1);
+    // Calculando densidade populacional e PIB per capita da carta 1
+    densidadePopulacional1 = populacao1 / area1;
+    pibPerCapita1 = (pib1 * 1000000000) / populacao1; // PIB em reais dividido pela população
 
-        printf("Qual é o nome da cidade?:\n");
-        scanf(" %[^\s]s", nome_cidade1);
+    // Recebendo os dados da carta 2
+    printf("\nCarta 2:\n");
+    printf("Estado: ");
+    scanf("%s", estado2);
+    printf("Codigo: ");
+    scanf("%s", codigo2);
+    printf("Nome da Cidade: ");
+    scanf(" %[^\n]", nomeCidade2);
+    printf("Populacao: ");
+    scanf("%d", &populacao2);
+    printf("Area (em km²): ");
+    scanf("%f", &area2);
+    printf("PIB (em bilhoes de reais): ");
+    scanf("%f", &pib2);
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &pontosTuristicos2);
 
-        printf("Qual a população?: \n");
-        scanf("%d", &populacao1);
+    // Calculando densidade populacional e PIB per capita da carta 2
+    densidadePopulacional2 = populacao2 / area2;
+    pibPerCapita2 = (pib2 * 1000000000) / populacao2;
 
-        printf("Qual a extensão da Área (em km²):  \n");
-        scanf("%f", &area1);
+    // Visualizando os dados da carta 1
+    printf("\nCarta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
-        printf("Qual o PIB?: \n");
-        scanf("%f", &pib1);
+    // Visualizando os dados da carta 2
+    printf("\nCarta 2:\n");
+    printf("Estado: %s\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
-        printf("Quantidade de Pontos Turísticos: \n");
-        scanf("%d", &ponto_turistico1);
-
-
- //Inserindo os dados da SEGUNDA carta:       
-
- printf("\nCarta 2: \n");
- printf("Digite a inicial do Estado: (Uma letra de 'A' a 'H') \n");
- scanf(" %c", &estado2); //o " " antes da variável garante que ele respeite a quebra de linha
-
- printf("Cadastre o código da Carta: (Ex: A01):\n");
- scanf("%s", codigo_carta2);
-
- printf("Qual é o nome da cidade?:\n");
- scanf(" %[^\s]s", nome_cidade2);
-
- printf("Qual a população?: \n");
- scanf("%d", &populacao2);
-
- printf("Qual a extensão da Área (em km²):  \n");
- scanf("%f", &area2);
-
- printf("Qual o PIB?: \n");
- scanf("%f", &pib2);
-
- printf("Quantidade de Pontos Turísticos: \n");
- scanf("%d", &ponto_turistico2);
-
-
- //////////  FINAL ////////////////////
-
-        //Retorno carta 1
-
-        printf(" \n");
-        printf("Carta 1:\n");
-        printf("Estado: %c\n", estado1);
-        printf("Código: %s\n", codigo_carta1);
-        printf("Nome da Cidade: %s\n", nome_cidade1);
-        printf("População: %d\n", populacao1);
-        printf("Área: %.2f km²\n", area1);
-        printf("PIB: %.2f Milhões de reais\n",pib1);
-        printf("Quantidade de Pontos Turísticos: %d\n", ponto_turistico1);
-
-       //Retorno carta 2
-        
-        printf(" \n");
-        printf("Carta 2:\n");
-        printf("Estado: %c\n", estado2);
-        printf("Código: %s\n",codigo_carta2);
-        printf("Nome da Cidade: %s\n",nome_cidade2);
-        printf("População: %d \n", populacao2);
-        printf("Área: %.2f km²\n", area2);
-        printf("PIB: %.2f Milhões de reais\n",pib2);
-        printf("Quantidade de Pontos Turísticos: %d\n", ponto_turistico2);
-
-return 0;
+    return 0; 
 }
